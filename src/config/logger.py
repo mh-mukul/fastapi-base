e4c842f1f2bc042ca0a095086c0b0bf8af4cd1ad
mh-mukul <mukulmehedy@gmail.com>
2026-04-30 05:53:07 +0600

    Refactor: enhance logging configuration and environment variable handling

--- src/config/logger.py
+++ src/config/logger.py
@@ -1,24 +1,31 @@
 import os
 import logging
 from logging.handlers import TimedRotatingFileHandler
+from dotenv import load_dotenv
 
+load_dotenv()
 
-if not os.path.exists("./logs"):
-    os.mkdir("./logs")
+LOG_TO_CONSOLE = bool(int(os.getenv("LOG_TO_CONSOLE", 1)))
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+LOG_DIR = os.getenv("LOG_DIR", "./logs")
+LOG_FILE_NAME = os.getenv("LOG_FILE_NAME", "app.log")
+LOG_BACKUP_COUNT = int(os.getenv("LOG_BACKUP_COUNT", 7))
 
-log_file = f"./logs/app.log"
-
-# Create a single logger instance
 logger = logging.getLogger("app_logger")
-logger.setLevel(logging.INFO)
+logger.setLevel(LOG_LEVEL)
+
+formatter = logging.Formatter(
+    "%(asctime)s - %(funcName)s - %(levelname)s - %(message)s"
+)
 
 if not logger.hasHandlers():
-    handler = TimedRotatingFileHandler(
-        log_file, when="midnight", interval=1, backupCount=7)
+    if LOG_TO_CONSOLE:
+        handler = logging.StreamHandler()
+    else:
+        os.makedirs(LOG_DIR, exist_ok=True)
+        log_file = os.path.join(LOG_DIR, LOG_FILE_NAME)
+        handler = TimedRotatingFileHandler(
+            log_file, when="midnight", interval=1, backupCount=LOG_BACKUP_COUNT)
 
-    formatter = logging.Formatter(
-        "%(asctime)s - %(funcName)s - %(levelname)s - %(message)s"
-    )
     handler.setFormatter(formatter)
-
     logger.addHandler(handler)
